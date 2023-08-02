b4f7c8a538739deff06f86a6e3ee81c1787ff4a4
bonnal-enzo <enzo.bonnal@voggt.com>
2023-08-02 21:06:10 +0200

    0.2.11

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='kioss',
-    version='0.2.10',
+    version='0.2.11',
     packages=['kioss'],
     url='http://github.com/bonnal-enzo/kioss',
     license='Apache 2.',
