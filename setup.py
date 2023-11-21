a1f704b9d28a067595ddd699a1bf08b50c6080ba
bonnal-enzo <enzo.bonnal@voggt.com>
2023-11-21 02:13:30 +0100

    0.4.7

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='kioss',
-    version='0.4.6',
+    version='0.4.7',
     packages=['kioss'],
     url='http://github.com/bonnal-enzo/kioss',
     license='Apache 2.',
     author='bonnal-enzo',
     author_email='bonnal.enzo.dev@gmail.com',
-    description='Keep I/O Simple and Stupid: Library providing a concise Iterator-based interface to write ETL pipelines.'
+    description='Keep I/O Simple and Stupid: Library providing a expressive Iterator-based interface to write ETL pipelines.'
 )
