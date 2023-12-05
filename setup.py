7230df406e805b51dc2fa1fba6a10f97257d577e
bonnal-enzo <enzo.bonnal@voggt.com>
2023-12-05 17:10:50 +0100

    0.6.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup # type: ignore
 
 setup(
     name='kioss',
-    version='0.6.7',
+    version='0.6.8',
     packages=find_packages(),
     package_data={"kioss": ["py.typed"]},
     url='http://github.com/bonnal-enzo/kioss',
