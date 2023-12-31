7a7c19a01b019debd515473e397bd1a4cde94d53
bonnal-enzo <enzo.bonnal@voggt.com>
2023-12-31 01:33:30 +0100

    _util: add LimitedYieldsIteratorWrapper

--- streamable/_util.py
+++ streamable/_util.py
@@ -69,3 +69,29 @@ def colorize_in_grey(s: str) -> str:
 
 def bold(s: str) -> str:
     return f"\033[1m{s}\033[0m"
+
+
+import threading
+import time
+
+
+class LimitedYieldsIteratorWrapper(Iterator[T]):
+    INIT_BACKFOFF = 0.0005
+
+    def __init__(self, iterator: Iterator[T], initial_available_yields: int):
+        self.iterator = iterator
+        self.available_yields = initial_available_yields
+
+    def __next__(self) -> T:
+        backoff = LimitedYieldsIteratorWrapper.INIT_BACKFOFF
+        while True:
+            with threading.Lock():
+                if self.available_yields > 0:
+                    self.available_yields -= 1
+                    return next(self.iterator)
+            time.sleep(backoff)
+            backoff *= 2
+
+    def increment_available_yields(self) -> None:
+        with threading.Lock():
+            self.available_yields += 1
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,17 +1,26 @@
 import unittest
 
-from streamable._util import sidify
+from streamable._util import LimitedYieldsIteratorWrapper, sidify
 
 
 class TestUtil(unittest.TestCase):
-    def test_sidify(self):
+    def test_sidify(self) -> None:
         f = lambda x: x**2
         self.assertEqual(f(2), 4)
         self.assertEqual(sidify(f)(2), 2)
 
         # test decoration
         @sidify
-        def f(x):
+        def g(x):
             return x**2
 
-        self.assertEqual(f(2), 2)
+        self.assertEqual(g(2), 2)
+
+    def test_controling_iterator_wrapper(self) -> None:
+        l = []
+        ciw = LimitedYieldsIteratorWrapper(
+            map(lambda i: l.append(i), range(10)), initial_available_yields=1
+        )
+        next(ciw)
+        self.assertListEqual(l, list(range(1)))
+        self.assertEqual(ciw.available_yields, 0)
