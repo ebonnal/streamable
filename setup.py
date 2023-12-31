d1c5f05ac437285b0dba1e21dc33a9245d2af47b
bonnal-enzo <enzo.bonnal@voggt.com>
2023-12-31 17:49:23 +0100

    0.1.0: replace '.iterate' by parameter 'raise_at_exception' in '.catch' and method '.exhaust' and improve docstrings

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup # type: ignore
 
 setup(
     name='streamable',
-    version='0.0.8',
+    version='0.1.0',
     packages=find_packages(),
     package_data={"streamable": ["py.typed"]},
     url='http://github.com/bonnal-enzo/streamable',
