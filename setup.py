c1d03aa3d33a9b3de017a682eb19daf032b9601e
bonnal-enzo <enzo.bonnal@voggt.com>
2024-01-03 15:55:09 +0100

    0.2.1: push map_exception into map function; improve '.observe'

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup # type: ignore
 
 setup(
     name='streamable',
-    version='0.2.0',
+    version='0.2.1',
     packages=find_packages(),
     package_data={"streamable": ["py.typed"]},
     url='http://github.com/bonnal-enzo/streamable',
