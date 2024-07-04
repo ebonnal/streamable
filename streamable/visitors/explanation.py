4e9ec2063b9c9bf6fc4b1362392ffd5018be56b7
bonnal-enzo <enzo.bonnal@voggt.com>
2024-07-04 16:21:41 +0200

    explanation: friendlify and base on method names

--- streamable/visitors/explanation.py
+++ streamable/visitors/explanation.py
@@ -1,5 +1,5 @@
 import textwrap
-from typing import cast
+from typing import Optional
 
 from streamable import stream
 from streamable.stream import (
@@ -19,12 +19,14 @@ from streamable.stream import (
 from streamable.visitor import Visitor
 
 
-def get_name(o: object) -> str:
+def friendly_string(o: object) -> str:
     if o is None:
         return "None"
     try:
         return o.__name__  # type: ignore
     except AttributeError:
+        if len(repr(o)) < 16:
+            return repr(o)
         return o.__class__.__name__ + "(...)"
 
 
@@ -34,67 +36,82 @@ class ExplanationVisitor(Visitor[str]):
         margin_step: int = 2,
     ) -> None:
         self.margin_step = margin_step
-        self.linking_symbol = "└" + "─" * (self.margin_step - 1) + "•"
+        self.linking_symbol = "└" + "─" * (self.margin_step - 1)
 
-    def _explanation(self, stream: stream.Stream, attributes_repr: str) -> str:
-        name = stream.__class__.__name__
-        stream_repr = f"{name}({attributes_repr})"
-        explanation = self.linking_symbol + stream_repr + "\n"
+    def _explanation(self, upstream: Optional[stream.Stream], stream_repr: str) -> str:
 
-        if stream.upstream is not None:
+        if upstream is not None:
+            explanation = self.linking_symbol + "•" + stream_repr + "\n"
             explanation += textwrap.indent(
-                stream.upstream.accept(self),
+                upstream.accept(self),
                 prefix=" " * self.margin_step,
             )
+        else:
+            explanation = self.linking_symbol + "┤" + stream_repr + "\n"
 
         return explanation
 
     def visit_stream(self, stream: Stream) -> str:
-        return self._explanation(stream, f"source={get_name(stream.source)}")
+        return self._explanation(
+            stream.upstream, f"Stream({friendly_string(stream.source)})"
+        )
 
     def visit_catch_stream(self, stream: CatchStream) -> str:
         return self._explanation(
-            stream,
-            f"kind={get_name(stream.kind)}, finally_raise={stream.finally_raise}",
+            stream.upstream,
+            f"catch({friendly_string(stream.kind)}, finally_raise={stream.finally_raise})",
         )
 
     def visit_filter_stream(self, stream: FilterStream) -> str:
-        return self._explanation(stream, f"keep={get_name(stream.keep)}")
+        return self._explanation(
+            stream.upstream, f"filter({friendly_string(stream.keep)})"
+        )
 
     def visit_flatten_stream(self, stream: FlattenStream) -> str:
-        return self._explanation(stream, f"concurrency={stream.concurrency}")
+        return self._explanation(
+            stream.upstream, f"flatten(concurrency={stream.concurrency})"
+        )
 
     def visit_foreach_stream(self, stream: ForeachStream) -> str:
         return self._explanation(
-            stream,
-            f"effect={get_name(stream.effect)}, concurrency={stream.concurrency}",
+            stream.upstream,
+            f"foreach({friendly_string(stream.effect)}, concurrency={stream.concurrency})",
         )
 
     def visit_aforeach_stream(self, stream: AForeachStream) -> str:
-        return self.visit_foreach_stream(cast(ForeachStream, stream))
+        return self._explanation(
+            stream.upstream,
+            f"aforeach({friendly_string(stream.effect)}, concurrency={stream.concurrency})",
+        )
 
     def visit_group_stream(self, stream: GroupStream) -> str:
         return self._explanation(
-            stream,
-            f"size={stream.size}, seconds={stream.seconds}, by={get_name(stream.by)}",
+            stream.upstream,
+            f"group(size={stream.size}, by={friendly_string(stream.by)}, seconds={stream.seconds})",
         )
 
     def visit_truncate_stream(self, stream: TruncateStream) -> str:
         return self._explanation(
-            stream, f"count={stream.count}, when={get_name(stream.when)}"
+            stream.upstream,
+            f"truncate(count={stream.count}, when={friendly_string(stream.when)})",
         )
 
     def visit_map_stream(self, stream: MapStream) -> str:
         return self._explanation(
-            stream,
-            f"transformation={get_name(stream.transformation)}, concurrency={stream.concurrency}",
+            stream.upstream,
+            f"map({friendly_string(stream.transformation)}, concurrency={stream.concurrency})",
         )
 
     def visit_amap_stream(self, stream: AMapStream) -> str:
-        return self.visit_map_stream(cast(MapStream, stream))
+        return self._explanation(
+            stream.upstream,
+            f"amap({friendly_string(stream.transformation)}, concurrency={stream.concurrency})",
+        )
 
     def visit_observe_stream(self, stream: ObserveStream) -> str:
-        return self._explanation(stream, f"what='{stream.what}'")
+        return self._explanation(stream.upstream, f"""observe("{stream.what}")""")
 
     def visit_throttle_stream(self, stream: ThrottleStream) -> str:
-        return self._explanation(stream, f"per_second={stream.per_second}")
+        return self._explanation(
+            stream.upstream, f"throttle(per_second={stream.per_second})"
+        )
