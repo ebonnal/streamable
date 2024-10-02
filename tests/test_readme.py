89128df396491f53c1247a26af394069ac25d955
ebonnal <bonnal.enzo.dev@gmail.com>
2024-10-02 20:38:25 +0100

    mypy test_etl_example

--- tests/test_readme.py
+++ tests/test_readme.py
@@ -197,3 +197,50 @@ class TestReadme(unittest.TestCase):
 
     def test_observe_example(self) -> None:
         observed_slow_integers: Stream[int] = slow_integers.observe("integers")
+
+    # fmt: off
+    def test_etl_example(self) -> None: # pragma: no cover
+        # for mypy typing check only
+        if not self:
+            import csv
+            from datetime import timedelta
+            import itertools
+            import requests
+            from streamable import Stream
+
+            with open("./quadruped_pokemons.csv", mode="w") as file:
+                fields = ["id", "name", "is_legendary", "base_happiness", "capture_rate"]
+                writer = csv.DictWriter(file, fields, extrasaction='ignore')
+                writer.writeheader()
+                (
+                    # Infinite Stream[int] of Pokemon ids starting from Pokémon #1: Bulbasaur
+                    Stream(itertools.count(1))
+                    # Limits to 16 requests per second to be friendly to our fellow PokéAPI devs
+                    .throttle(per_second=16)
+                    # GETs pokemons concurrently using a pool of 8 threads
+                    .map(lambda poke_id: f"https://pokeapi.co/api/v2/pokemon-species/{poke_id}")
+                    .map(requests.get, concurrency=8)
+                    .foreach(requests.Response.raise_for_status)
+                    .map(requests.Response.json)
+                    # Stops the iteration when reaching the 1st pokemon of the 4th generation
+                    .truncate(when=lambda poke: poke["generation"]["name"] == "generation-iv")
+                    .observe("pokemons")
+                    # Keeps only quadruped Pokemons
+                    .filter(lambda poke: poke["shape"]["name"] == "quadruped")
+                    .observe("quadruped pokemons")
+                    # Catches errors due to None "generation" or "shape"
+                    .catch(
+                        TypeError,
+                        when=lambda error: str(error) == "'NoneType' object is not subscriptable"
+                    )
+                    # Writes a batch of pokemons every 5 seconds to the CSV file
+                    .group(interval=timedelta(seconds=5))
+                    .foreach(writer.writerows)
+                    .flatten()
+                    .observe("written pokemons")
+                    # Catches exceptions and raises the 1st one at the end of the iteration
+                    .catch(finally_raise=True)
+                    # Actually triggers an iteration (the lines above define lazy operations)
+                    .count()
+                )
+    # fmt: on
