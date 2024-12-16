460250c4b1ccbc512214fb5057d4ee5f8e700d7d
ebonnal <bonnal.enzo.dev@gmail.com>
2024-12-16 21:31:51 +0000

    iterators: rename buffer_size->buffersize

--- tests/test_iterators.py
+++ tests/test_iterators.py
@@ -7,14 +7,14 @@ class TestIterators(unittest.TestCase):
     def test_validation(self):
         with self.assertRaisesRegex(
             ValueError,
-            "`buffer_size` should be greater or equal to 1, but got 0.",
-            msg="`_OSConcurrentMapIterable` constructor should raise for non-positive buffer_size",
+            "`buffersize` should be greater or equal to 1, but got 0.",
+            msg="`_OSConcurrentMapIterable` constructor should raise for non-positive buffersize",
         ):
             _OSConcurrentMapIterable(
                 iterator=iter([]),
                 transformation=str,
                 concurrency=1,
-                buffer_size=0,
+                buffersize=0,
                 ordered=True,
                 via="thread",
             )
