551352fd46597b8a1be4b4adc1926282f93ed590
Enzo Bonnal <bonnal.enzo.dev@gmail.com>
2025-01-23 15:10:56 +0000

    `.skip`/`.truncate`: make no-op if args are `None` (closes #57); `.skip`: ok when both `count` and `until` are set (#59)

--- streamable/util/validationtools.py
+++ streamable/util/validationtools.py
@@ -1,6 +1,5 @@
 import datetime
-import sys
-from typing import Any, Callable, Iterator, Optional, TypeVar
+from typing import Iterator, Optional, TypeVar
 
 T = TypeVar("T")
 
@@ -43,8 +42,11 @@ def validate_group_interval(interval: Optional[datetime.timedelta]) -> None:
 def validate_count(count: int):
     if count < 0:
         raise ValueError(f"`count` must be >= 0 but got {count}")
-    if count >= sys.maxsize:
-        raise ValueError(f"`count` must be < sys.maxsize but got {count}")
+
+
+def validate_optional_count(count: Optional[int]):
+    if count is not None:
+        validate_count(count)
 
 
 def validate_throttle_per_period(per_period_arg_name: str, value: int) -> None:
@@ -55,25 +57,3 @@ def validate_throttle_per_period(per_period_arg_name: str, value: int) -> None:
 def validate_throttle_interval(interval: datetime.timedelta) -> None:
     if interval < datetime.timedelta(0):
         raise ValueError(f"`interval` must be >= 0 but got {repr(interval)}")
-
-
-def validate_truncate_args(
-    count: Optional[int] = None, when: Optional[Callable[[T], Any]] = None
-) -> None:
-    if count is None:
-        if when is None:
-            raise ValueError("`count` and `when` cannot both be None")
-    else:
-        validate_count(count)
-
-
-def validate_skip_args(
-    count: Optional[int] = None, until: Optional[Callable[[T], Any]] = None
-) -> None:
-    if count is None:
-        if until is None:
-            raise ValueError("`count` and `until` cannot both be None")
-    else:
-        if until is not None:
-            raise ValueError("`count` and `until` cannot both be set")
-        validate_count(count)
