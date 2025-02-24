9736cca8cb664e4edaba2f4fec50ad9846b2a617
Enzo Bonnal <bonnal.enzo.dev@gmail.com>
2025-02-24 15:28:06 +0000

    override `__eq__` (closes #32) (#69)

--- streamable/stream.py
+++ streamable/stream.py
@@ -101,10 +101,31 @@ class Stream(Iterable[T]):
 
         return self.accept(ReprVisitor())
 
+    def __eq__(self, other: Any) -> bool:
+        """
+        Checks if this stream is equal to `other`, meaning they apply the same operations to the same source.
+
+        Returns:
+            bool: True if this stream is equal to `other`.
+        """
+        from streamable.visitors.equality import EqualityVisitor
+
+        return self.accept(EqualityVisitor(other))
+
     def __str__(self) -> str:
         from streamable.visitors.representation import StrVisitor
 
         return self.accept(StrVisitor())
+
+    def __call__(self) -> "Stream[T]":
+        """
+        Iterates over this stream until exhaustion.
+
+        Returns:
+            Stream[T]: self.
+        """
+        self.count()
+        return self
 
     def accept(self, visitor: "Visitor[V]") -> V:
         """
@@ -151,16 +172,6 @@ class Stream(Iterable[T]):
         """
 
         return sum(1 for _ in self)
-
-    def __call__(self) -> "Stream[T]":
-        """
-        Iterates over this stream until exhaustion.
-
-        Returns:
-            Stream[T]: self.
-        """
-        self.count()
-        return self
 
     def display(self, level: int = logging.INFO) -> "Stream[T]":
         """
