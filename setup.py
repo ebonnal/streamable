e7995acf12bbd3385e5eddee85c9682bbba1ae02
ebonnal <bonnal.enzo.dev@gmail.com>
2025-03-13 01:02:53 +0000

    CD: remove tests

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from version import __version__
 setup(
     name="streamable",
     version=__version__,
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests"]),
     package_data={"streamable": ["py.typed"]},
     url="http://github.com/ebonnal/streamable",
     license="Apache 2.",
