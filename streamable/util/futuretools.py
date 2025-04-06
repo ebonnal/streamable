c423b7ff72c0689e21fbc7579d6d96479601cd70
ebonnal <bonnal.enzo.dev@gmail.com>
2025-04-06 06:34:33 +0100

    add pending percent

--- streamable/util/futuretools.py
+++ streamable/util/futuretools.py
@@ -3,7 +3,7 @@ from abc import ABC, abstractmethod
 from collections import deque
 from concurrent.futures import Future
 from contextlib import suppress
-from typing import Awaitable, Deque, Iterator, Sized, Type, TypeVar, cast
+from typing import Awaitable, Collection, Deque, Iterable, Iterator, Set, Sized, Type, TypeVar, cast
 
 with suppress(ImportError):
     from streamable.util.protocols import Queue
@@ -15,17 +15,23 @@ class FutureResultCollection(Iterator[T], Sized, ABC):
     """
     Iterator over added futures' results. Supports adding new futures after iteration started.
     """
+    _futures: Collection["Future[T]"]
 
     @abstractmethod
     def add(self, future: "Future[T]") -> None: ...
 
+    def num_pending(self) -> int:
+        return sum(1 for future in self._futures if not future.done())
+    
+    def pending_percent(self) -> float:
+        return self.num_pending() / len(self)
+
+    def __len__(self) -> int:
+        return len(self._futures)
 
 class DequeFutureResultCollection(FutureResultCollection[T]):
     def __init__(self) -> None:
         self._futures: Deque["Future[T]"] = deque()
-
-    def __len__(self) -> int:
-        return len(self._futures)
 
     def add(self, future: "Future[T]") -> None:
         return self._futures.append(future)
@@ -33,17 +39,14 @@ class DequeFutureResultCollection(FutureResultCollection[T]):
 
 class CallbackFutureResultCollection(FutureResultCollection[T]):
     def __init__(self) -> None:
-        self._n_futures = 0
-
-    def __len__(self) -> int:
-        return self._n_futures
+        self._futures: Set["Future[T]"] = set()
 
     @abstractmethod
     def _done_callback(self, future: "Future[T]") -> None: ...
 
     def add(self, future: "Future[T]") -> None:
         future.add_done_callback(self._done_callback)
-        self._n_futures += 1
+        self._futures.add(future)
 
 
 class FIFOOSFutureResultCollection(DequeFutureResultCollection[T]):
@@ -66,11 +69,10 @@ class FDFOOSFutureResultCollection(CallbackFutureResultCollection[T]):
 
     def _done_callback(self, future: "Future[T]") -> None:
         self._results.put(future.result())
+        self._futures.remove(future)
 
     def __next__(self) -> T:
-        result = self._results.get()
-        self._n_futures -= 1
-        return result
+        return self._results.get()
 
 
 class FIFOAsyncFutureResultCollection(DequeFutureResultCollection[T]):
@@ -100,9 +102,9 @@ class FDFOAsyncFutureResultCollection(CallbackFutureResultCollection[T]):
 
     def _done_callback(self, future: "Future[T]") -> None:
         self._waiter.set_result(future.result())
+        self._futures.remove(future)
 
     def __next__(self) -> T:
         result = self.event_loop.run_until_complete(self._waiter)
-        self._n_futures -= 1
         self._waiter = self.event_loop.create_future()
         return result
