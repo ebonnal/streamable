9fd6d637a1f2902482a82f78455307cd21dacaa2
ebonnal <bonnal.enzo.dev@gmail.com>
2025-05-11 15:36:47 +0100

    remove from toplevel get_logger/get_event_loop

--- streamable/__init__.py
+++ streamable/__init__.py
@@ -1,4 +1,2 @@
 from streamable.stream import Stream
-from streamable.util.asynctools import get_event_loop
 from streamable.util.functiontools import star
-from streamable.util.loggertools import get_logger
