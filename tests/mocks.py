de0cf477fed76dd915d8373a1ce6b64c238979c3
ebonnal <bonnal.enzo.dev@gmail.com>
2025-11-05 21:39:44 +0000

    README: replace `requests` by `httpx` in sync ETL snippet

--- tests/mocks.py
+++ tests/mocks.py
@@ -1,14 +1,13 @@
 import json
-from typing import Any, Dict, List, Union
+from typing import Any, Dict, List
 from unittest.mock import Mock
 import httpx
-import requests
 
 with open("tests/pokemons.json") as pokemon_sample:
     POKEMONS: List[Dict[str, Any]] = json.loads(pokemon_sample.read())
 
 
-def get_poke(url: str) -> Union[requests.Response, httpx.Response]:
+def get_poke(url: str) -> httpx.Response:
     poke_id = int(url.split("/")[-1])
     response = Mock()
     response.text = json.dumps(POKEMONS[poke_id - 1])
@@ -17,5 +16,5 @@ def get_poke(url: str) -> Union[requests.Response, httpx.Response]:
     return response
 
 
-async def async_get_poke(url: str) -> Union[requests.Response, httpx.Response]:
+async def async_get_poke(url: str) -> httpx.Response:
     return get_poke(url)
