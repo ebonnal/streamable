7911130071927d7a004adf79078954ab3f6b2de6
ebonnal <bonnal.enzo.dev@gmail.com>
2025-11-15 18:05:04 +0000

    finalize pytestification

---
+++ tests/test_validate.py
@@ -0,0 +1,39 @@
+import pytest
+
+from streamable import Stream
+from tests.utils import (
+    async_identity,
+    identity,
+    src,
+)
+
+
+@pytest.mark.parametrize(
+    "method, args",
+    (
+        (Stream.map, [identity]),
+        (Stream.amap, [async_identity]),
+        (Stream.foreach, [identity]),
+        (Stream.aforeach, [identity]),
+        (Stream.flatten, []),
+        (Stream.aflatten, []),
+    ),
+)
+def test_validate_concurrency(method, args) -> None:
+    stream = Stream(src)
+    # should be raising TypeError for non-int concurrency.
+    with pytest.raises(TypeError):
+        method(stream, *args, concurrency="1")
+    # should be raising ValueError for concurrency=0.
+    with pytest.raises(ValueError):
+        method(stream, *args, concurrency=0)
+
+
+@pytest.mark.parametrize("method", (Stream.map, Stream.foreach))
+def test_validate_via(method) -> None:
+    # must raise a TypeError for invalid via
+    with pytest.raises(
+        TypeError,
+        match="`via` must be 'thread' or 'process' but got 'foo'",
+    ):
+        method(Stream(src), identity, via="foo")
