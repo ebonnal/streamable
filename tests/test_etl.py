c3239fd576dc9eb29ea18f563f7863a7e269f366
ebonnal <bonnal.enzo.dev@gmail.com>
2026-01-14 22:33:37 +0000

    tests: clean up test_readme

---
+++ tests/test_etl.py
@@ -0,0 +1,83 @@
+from pathlib import Path
+from datetime import timedelta
+import httpx
+
+import pytest
+
+
+def test_etl_example(tmp_path: Path) -> None:  # pragma: no cover
+    import csv
+    from itertools import count
+    from streamable import stream
+
+    with open(tmp_path / "quadruped_pokemons.csv", mode="w") as file:
+        fields = ["id", "name", "is_legendary", "base_happiness", "capture_rate"]
+        writer = csv.DictWriter(file, fields, extrasaction="ignore")
+        writer.writeheader()
+
+        pipeline = (
+            # Infinite stream[int] of Pokemon ids starting from Pokémon #1: Bulbasaur
+            stream(count(1))
+            # Limit to 16 requests per second to be friendly to our fellow PokéAPI devs
+            .throttle(16, per=timedelta(seconds=1))
+            # GET pokemons concurrently using a pool of 8 threads
+            .map(lambda poke_id: f"https://pokeapi.co/api/v2/pokemon-species/{poke_id}")
+            .map(httpx.Client().get, concurrency=8)
+            .do(httpx.Response.raise_for_status)
+            .map(httpx.Response.json)
+            # Stop when reaching the 1st pokemon of the 4th generation
+            .take(until=lambda poke: poke["generation"]["name"] == "generation-iv")
+            .observe("pokemons")
+            # Keep only quadruped Pokemons
+            .filter(lambda poke: poke["shape"]["name"] == "quadruped")
+            # Write a batch of pokemons every 5 seconds to the CSV file
+            .group(every=timedelta(seconds=5))
+            .do(writer.writerows)
+            .flatten()
+            .observe("written pokemons")
+        )
+
+        # Call the stream to consume it (as an Iterable)
+        # without collecting its elements
+        pipeline()
+
+
+@pytest.mark.asyncio
+async def test_async_etl_example(tmp_path: Path) -> None:  # pragma: no cover
+    import csv
+    from itertools import count
+    from streamable import stream
+
+    with open(tmp_path / "quadruped_pokemons.csv", mode="w") as file:
+        fields = ["id", "name", "is_legendary", "base_happiness", "capture_rate"]
+        writer = csv.DictWriter(file, fields, extrasaction="ignore")
+        writer.writeheader()
+
+        pipeline = (
+            # Infinite stream[int] of Pokemon ids starting from Pokémon #1: Bulbasaur
+            stream(count(1))
+            # Limit to 16 requests per second to be friendly to our fellow PokéAPI devs
+            .throttle(16, per=timedelta(seconds=1))
+            # GET pokemons via 8 concurrent coroutines
+            .map(lambda poke_id: f"https://pokeapi.co/api/v2/pokemon-species/{poke_id}")
+            .map(httpx.AsyncClient().get, concurrency=8)
+            .do(httpx.Response.raise_for_status)
+            .map(httpx.Response.json)
+            # Stop when reaching the 1st pokemon of the 4th generation
+            .take(until=lambda poke: poke["generation"]["name"] == "generation-iv")
+            .observe("pokemons")
+            # Keep only quadruped Pokemons
+            .filter(lambda poke: poke["shape"]["name"] == "quadruped")
+            # Write a batch of pokemons every 5 seconds to the CSV file
+            .group(every=timedelta(seconds=5))
+            .do(writer.writerows)
+            .flatten()
+            .observe("written pokemons")
+        )
+
+        # await the stream to consume it (as an AsyncIterable)
+        # without collecting its elements
+        await pipeline
+
+
+# fmt: on
--- tests/test_readme.py
+++ tests/test_readme.py
@@ -5,7 +5,7 @@ import logging
 from pathlib import Path
 import time
 from datetime import timedelta
-from typing import Any, Dict, Iterator, List, Tuple, TypeVar
+from typing import Any, Dict, List, Tuple
 import httpx
 from httpx import AsyncClient, Response, HTTPStatusError
 
@@ -13,8 +13,6 @@ import pytest
 import respx
 
 from streamable._stream import stream
-
-ints: stream[int] = stream(range(10))
 
 
 pokemons: stream[str] = (
@@ -26,8 +24,6 @@ pokemons: stream[str] = (
     .catch(HTTPStatusError, do=logging.error)
     .map(lambda poke: poke.json()["name"])
 )
-
-three_ints_per_second: stream[int] = ints.throttle(5, per=timedelta(seconds=1))
 
 POKEMONS = [
     "bulbasaur",
@@ -41,12 +37,9 @@ POKEMONS = [
     "blastoise",
 ]
 
-T = TypeVar("T")
-
 
 def test_iterate() -> None:
     assert list(pokemons) == POKEMONS
-    assert [poke for poke in pokemons] == POKEMONS
 
 
 @pytest.mark.asyncio
@@ -55,12 +48,12 @@ async def test_aiterate() -> None:
 
 
 def test_map_example() -> None:
-    str_ints: stream[str] = ints.map(str)
+    str_ints: stream[str] = stream(range(10)).map(str)
 
     assert list(str_ints) == ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 
-def test_thread_concurrent_map_example() -> None:
+def test_map_example_thread_concurrency() -> None:
     pokemons: stream[str] = (
         stream(range(1, 4))
         .map(lambda i: f"https://pokeapi.co/api/v2/pokemon-species/{i}")
@@ -70,17 +63,20 @@ def test_thread_concurrent_map_example() -> None:
     assert list(pokemons) == ["bulbasaur", "ivysaur", "venusaur"]
 
 
-def test_process_concurrent_map_example() -> None:
-    with ProcessPoolExecutor(10) as processes:
+def test_map_example_process_concurrency() -> None:
+    with ProcessPoolExecutor(max_workers=10) as processes:
         state: List[int] = []
         # ints are mapped
-        assert list(ints.map(state.append, concurrency=processes)) == [None] * 10
+        assert (
+            list(stream(range(10)).map(state.append, concurrency=processes))
+            == [None] * 10
+        )
         # but the `state` of the main process is not mutated
         assert state == []
 
 
 @pytest.mark.asyncio
-async def test_async_map_example_async() -> None:
+async def test_map_example_async_concurrency_aiter() -> None:
     pokemons: stream[str] = (
         stream(range(1, 4))
         .map(lambda i: f"https://pokeapi.co/api/v2/pokemon-species/{i}")
@@ -91,7 +87,7 @@ async def test_async_map_example_async() -> None:
     assert [name async for name in pokemons] == ["bulbasaur", "ivysaur", "venusaur"]
 
 
-def test_async_map_example_sync() -> None:
+def test_map_example_async_concurrency_iter() -> None:
     pokemons: stream[str] = (
         stream(range(1, 4))
         .map(lambda i: f"https://pokeapi.co/api/v2/pokemon-species/{i}")
@@ -123,14 +119,14 @@ def test_starmap_example() -> None:
 
 def test_do_example() -> None:
     state: List[int] = []
-    ints_into_state: stream[int] = ints.do(state.append)
+    ints_into_state: stream[int] = stream(range(10)).do(state.append)
 
     assert list(ints_into_state) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     assert state == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
 def test_filter_example() -> None:
-    even_ints: stream[int] = ints.filter(lambda n: n % 2 == 0)
+    even_ints: stream[int] = stream(range(10)).filter(lambda n: n % 2 == 0)
 
     assert list(even_ints) == [0, 2, 4, 6, 8]
 
@@ -138,14 +134,18 @@ def test_filter_example() -> None:
 def test_throttle_example() -> None:
     from datetime import timedelta
 
-    three_ints_per_second: stream[int] = ints.throttle(3, per=timedelta(seconds=1))
+    three_ints_per_second: stream[int] = stream(range(10)).throttle(
+        3, per=timedelta(seconds=1)
+    )
 
     start = time.perf_counter()
     # takes 3s: ceil(10 ints / 3 per_second) - 1
     assert list(three_ints_per_second) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     assert 2.99 < time.perf_counter() - start < 3.25
 
-    ints_every_100_millis = ints.throttle(1, per=timedelta(milliseconds=100))
+    ints_every_100_millis = stream(range(10)).throttle(
+        1, per=timedelta(milliseconds=100)
+    )
 
     start = time.perf_counter()
     # takes 900 millis: (10 ints - 1) * 100 millis
@@ -154,19 +154,21 @@ def test_throttle_example() -> None:
 
 
 def test_group_example() -> None:
-    int_batches: stream[List[int]] = ints.group(5)
+    int_batches: stream[List[int]] = stream(range(10)).group(5)
 
     assert list(int_batches) == [[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]]
 
     from datetime import timedelta
 
-    int_1sec_batches: stream[List[int]] = ints.throttle(
-        2, per=timedelta(seconds=1)
-    ).group(every=timedelta(seconds=0.99))
+    int_1sec_batches: stream[List[int]] = (
+        stream(range(10))
+        .throttle(2, per=timedelta(seconds=1))
+        .group(every=timedelta(seconds=0.99))
+    )
 
     assert list(int_1sec_batches) == [[0, 1, 2], [3, 4], [5, 6], [7, 8], [9]]
 
-    ints_by_parity: stream[Tuple[str, List[int]]] = ints.group(
+    ints_by_parity: stream[Tuple[str, List[int]]] = stream(range(10)).group(
         by=lambda n: "odd" if n % 2 else "even"
     )
 
@@ -192,8 +194,8 @@ def test_flatten_example() -> None:
 
 
 def test_catch_example() -> None:
-    inverses: stream[float] = ints.map(lambda n: round(1 / n, 2)).catch(
-        ZeroDivisionError
+    inverses: stream[float] = (
+        stream(range(10)).map(lambda n: round(1 / n, 2)).catch(ZeroDivisionError)
     )
 
     assert list(inverses) == [1.0, 0.5, 0.33, 0.25, 0.2, 0.17, 0.14, 0.12, 0.11]
@@ -218,14 +220,18 @@ def test_catch_example() -> None:
         assert list(resolvable_domains) == ["github.com", "google.com"]
 
     errors: List[Exception] = []
-    inverses = ints.map(lambda n: round(1 / n, 2)).catch(
-        ZeroDivisionError, do=errors.append
+    inverses = (
+        stream(range(10))
+        .map(lambda n: round(1 / n, 2))
+        .catch(ZeroDivisionError, do=errors.append)
     )
     assert list(inverses) == [1.0, 0.5, 0.33, 0.25, 0.2, 0.17, 0.14, 0.12, 0.11]
     assert len(errors) == 1
 
-    inverses = ints.map(lambda n: round(1 / n, 2)).catch(
-        ZeroDivisionError, replace=lambda e: float("inf")
+    inverses = (
+        stream(range(10))
+        .map(lambda n: round(1 / n, 2))
+        .catch(ZeroDivisionError, replace=lambda e: float("inf"))
     )
 
     assert list(inverses) == [
@@ -241,27 +247,31 @@ def test_catch_example() -> None:
         0.11,
     ]
 
-    inverses = ints.map(lambda n: round(1 / n, 2)).catch(ZeroDivisionError, stop=True)
+    inverses = (
+        stream(range(10))
+        .map(lambda n: round(1 / n, 2))
+        .catch(ZeroDivisionError, stop=True)
+    )
 
     assert list(inverses) == []
 
 
 def test_take_example() -> None:
-    first_5_ints: stream[int] = ints.take(5)
+    first_5_ints: stream[int] = stream(range(10)).take(5)
 
     assert list(first_5_ints) == [0, 1, 2, 3, 4]
 
-    first_5_ints = ints.take(until=lambda n: n == 5)
+    first_5_ints = stream(range(10)).take(until=lambda n: n == 5)
 
     assert list(first_5_ints) == [0, 1, 2, 3, 4]
 
 
 def test_skip_example() -> None:
-    ints_after_5: stream[int] = ints.skip(5)
+    ints_after_5: stream[int] = stream(range(10)).skip(5)
 
     assert list(ints_after_5) == [5, 6, 7, 8, 9]
 
-    ints_after_5 = ints.skip(until=lambda n: n >= 5)
+    ints_after_5 = stream(range(10)).skip(until=lambda n: n >= 5)
 
     assert list(ints_after_5) == [5, 6, 7, 8, 9]
 
@@ -278,18 +288,18 @@ def test_distinct_example() -> None:
 
 def test_buffer_example() -> None:
     pulled: list[int] = []
-    buffered_ints = ints.do(pulled.append).buffer(5)
+    buffered_ints = stream(range(10)).do(pulled.append).buffer(5)
     assert next(iter(buffered_ints)) == 0
     assert pulled == [0, 1, 2, 3, 4, 5]
 
 
 def test_observe_example() -> None:
-    observed_ints: stream[int] = ints.observe("ints")
+    observed_ints: stream[int] = stream(range(10)).observe("ints")
     assert list(observed_ints) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
 def test_plus_example() -> None:
-    assert list(ints + ints) == [
+    assert list(stream(range(10)) + stream(range(10))) == [
         0,
         1,
         2,
@@ -349,7 +359,7 @@ async def test_afunc_source() -> None:
 
 def test_call_example() -> None:
     state: List[int] = []
-    pipeline: stream[int] = ints.do(state.append)
+    pipeline: stream[int] = stream(range(10)).do(state.append)
     assert pipeline() is pipeline
     assert state == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
@@ -357,29 +367,15 @@ def test_call_example() -> None:
 @pytest.mark.asyncio
 async def test_await_example() -> None:
     state: List[int] = []
-    pipeline: stream[int] = ints.do(state.append)
+    pipeline: stream[int] = stream(range(10)).do(state.append)
     assert pipeline is await pipeline
     assert state == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-
-def test_non_stopping_exceptions_example() -> None:
-    from contextlib import suppress
-
-    casted_ints: Iterator[int] = iter(stream("0123_56789").map(int).group(3).flatten())
-    collected_casted_ints: List[int] = []
-
-    with suppress(ValueError):
-        collected_casted_ints.extend(casted_ints)
-    assert collected_casted_ints == [0, 1, 2, 3]
-
-    collected_casted_ints.extend(casted_ints)
-    assert collected_casted_ints == [0, 1, 2, 3, 5, 6, 7, 8, 9]
 
 
 def test_pipe(tmp_path: Path) -> None:
     import polars as pl
 
-    pokemons.pipe(pl.DataFrame, schema=["name"]).write_csv(tmp_path / "ints.parquet")
+    pokemons.pipe(pl.DataFrame, schema=["name"]).write_csv(tmp_path / "pokemons.csv")
 
 
 def test_cast() -> None:
@@ -389,85 +385,3 @@ def test_cast() -> None:
         .cast(Dict[str, str])  # stream[Dict[str, str]]
     )
     assert list(_dicts) == [{"foo": "bar"}, {"foo": "baz"}]
-
-
-def test_etl_example(tmp_path: Path) -> None:  # pragma: no cover
-    import csv
-    from datetime import timedelta
-    from itertools import count
-    import httpx
-    from streamable import stream
-
-    with open(tmp_path / "quadruped_pokemons.csv", mode="w") as file:
-        fields = ["id", "name", "is_legendary", "base_happiness", "capture_rate"]
-        writer = csv.DictWriter(file, fields, extrasaction="ignore")
-        writer.writeheader()
-
-        pipeline = (
-            # Infinite stream[int] of Pokemon ids starting from Pokémon #1: Bulbasaur
-            stream(count(1))
-            # Limit to 16 requests per second to be friendly to our fellow PokéAPI devs
-            .throttle(16, per=timedelta(seconds=1))
-            # GET pokemons concurrently using a pool of 8 threads
-            .map(lambda poke_id: f"https://pokeapi.co/api/v2/pokemon-species/{poke_id}")
-            .map(httpx.Client().get, concurrency=8)
-            .do(httpx.Response.raise_for_status)
-            .map(httpx.Response.json)
-            # Stop when reaching the 1st pokemon of the 4th generation
-            .take(until=lambda poke: poke["generation"]["name"] == "generation-iv")
-            .observe("pokemons")
-            # Keep only quadruped Pokemons
-            .filter(lambda poke: poke["shape"]["name"] == "quadruped")
-            # Write a batch of pokemons every 5 seconds to the CSV file
-            .group(every=timedelta(seconds=5))
-            .do(writer.writerows)
-            .flatten()
-            .observe("written pokemons")
-        )
-
-        # Call the stream to consume it (as an Iterable)
-        # without collecting its elements
-        pipeline()
-
-
-@pytest.mark.asyncio
-async def test_async_etl_example(tmp_path: Path) -> None:  # pragma: no cover
-    import csv
-    from datetime import timedelta
-    from itertools import count
-    import httpx
-    from streamable import stream
-
-    with open(tmp_path / "quadruped_pokemons.csv", mode="w") as file:
-        fields = ["id", "name", "is_legendary", "base_happiness", "capture_rate"]
-        writer = csv.DictWriter(file, fields, extrasaction="ignore")
-        writer.writeheader()
-
-        pipeline = (
-            # Infinite stream[int] of Pokemon ids starting from Pokémon #1: Bulbasaur
-            stream(count(1))
-            # Limit to 16 requests per second to be friendly to our fellow PokéAPI devs
-            .throttle(16, per=timedelta(seconds=1))
-            # GET pokemons via 8 concurrent coroutines
-            .map(lambda poke_id: f"https://pokeapi.co/api/v2/pokemon-species/{poke_id}")
-            .map(httpx.AsyncClient().get, concurrency=8)
-            .do(httpx.Response.raise_for_status)
-            .map(httpx.Response.json)
-            # Stop when reaching the 1st pokemon of the 4th generation
-            .take(until=lambda poke: poke["generation"]["name"] == "generation-iv")
-            .observe("pokemons")
-            # Keep only quadruped Pokemons
-            .filter(lambda poke: poke["shape"]["name"] == "quadruped")
-            # Write a batch of pokemons every 5 seconds to the CSV file
-            .group(every=timedelta(seconds=5))
-            .do(writer.writerows)
-            .flatten()
-            .observe("written pokemons")
-        )
-
-        # await the stream to consume it (as an AsyncIterable)
-        # without collecting its elements
-        await pipeline
-
-
-# fmt: on
