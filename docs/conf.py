ffd52cb254f0f2fb7ecf9be6b545ff25b12e8355
ebonnal <bonnal.enzo.dev@gmail.com>
2026-02-19 22:31:01 +0000

    docs: add favicon

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 import importlib.metadata
 from datetime import datetime
 
-project = "༄ <code>streamable</code>"
+project = "<code>streamable</code>"
 author = "ebonnal"
 copyright = f"{datetime.now():%Y}, {author}"
 
@@ -41,8 +41,10 @@ exclude_patterns = [
 ]
 
 templates_path = ["_templates"]
+html_static_path = ["_static"]
 
 html_theme = "furo"
+html_favicon = "_static/favicon.png"
 
 html_theme_options = {
     "announcement": """<a href="https://github.com/ebonnal/streamable/" target="_blank"><svg aria-hidden="true" focusable="false" class="octicon octicon-mark-github" viewBox="0 0 24 24" width="32" height="32" fill="currentColor" display="inline-block" overflow="visible" style="vertical-align:text-bottom"><path d="M12 1C5.923 1 1 5.923 1 12c0 4.867 3.149 8.979 7.521 10.436.55.096.756-.233.756-.522 0-.262-.013-1.128-.013-2.049-2.764.509-3.479-.674-3.699-1.292-.124-.317-.66-1.293-1.127-1.554-.385-.207-.936-.715-.014-.729.866-.014 1.485.797 1.691 1.128.99 1.663 2.571 1.196 3.204.907.096-.715.385-1.196.701-1.471-2.448-.275-5.005-1.224-5.005-5.432 0-1.196.426-2.186 1.128-2.956-.111-.275-.496-1.402.11-2.915 0 0 .921-.288 3.024 1.128a10.193 10.193 0 0 1 2.75-.371c.936 0 1.871.123 2.75.371 2.104-1.43 3.025-1.128 3.025-1.128.605 1.513.221 2.64.111 2.915.701.77 1.127 1.747 1.127 2.956 0 4.222-2.571 5.157-5.019 5.432.399.344.743 1.004.743 2.035 0 1.471-.014 2.654-.014 3.025 0 .289.206.632.756.522C19.851 20.979 23 16.854 23 12c0-6.077-4.922-11-11-11Z"></path></svg></a>""",
